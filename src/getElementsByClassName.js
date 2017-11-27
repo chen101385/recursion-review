7982e6c2556976e2951d33cc55723c02094e08fa
chen101385 <chen1385@gmail.com>
2017-11-27 12:38:35 -0800

    finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  searchTree(document.body);
+  
+  // inner recursive function(parameter1) {
+  function searchTree(node) {
+    //has class?
+    if (node.classList) {
+      //if yes, search through clases for className
+      for (var i = 0; i < node.classList.length; i++) {
+        var classItem = node.classList[i];
+        //if match is found
+        if(classItem === className) {
+          result.push(node)
+        }
+      }
+    }
+
+    //next check for children
+    if (node.childNodes) {
+      //run test above for each child
+      for (var j = 0; j < node.childNodes.length; j++) {
+        var child = node.childNodes[j];
+        searchTree(child);
+      }
+    }
+  }
+  
+
+//return array
+return result;
+
 };
