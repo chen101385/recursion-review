0bd759a66d399073880ec59fda95cba216fdfaeb
chen101385 <chen1385@gmail.com>
2017-11-27 16:43:51 -0800

    File contains test cases

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,39 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+//UNSTRINGIFY  
+//Is input string?
+//if(typeof json === 'string') {
+  //json.replace("'", '')
+
+//Is object?
+json.indexOf("{")
+
+
+//Is array?
+
+json.indexOf("[")
+
+
+}
+
+//Is string?
+json.indexOf(""'")
+
+
+//Is all other cases?
+
+json.indexOf()
+
+
+return "{" + aggregator + "}";
+
+
+
+//return object literal or primitive e.g. boolean, number, 
+
+
+
+
+
 };
